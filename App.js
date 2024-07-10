4986c7d176e95b7cc37d5c8c1ef340d9e758a4e2
inanderinakin <inandakin@gmail.com>
2024-07-10 21:02:33 +0300

    added the tab system, made some design changes, changed text to icons at the bottom navbar

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import {StyleSheet, View, TouchableOpacity, Text, Dimensions, Image, ActivityIndicator} from "react-native";
+import {StyleSheet, View, TouchableOpacity, Text, Dimensions, Image, ActivityIndicator, Linking, Platform} from "react-native";
 import * as Location from "expo-location";
 import MapView, { Marker } from "react-native-maps";
 import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
 
 const WaitingDriverScreen = () => {
   const [currentLocation, setCurrentLocation] = useState(null);
@@ -33,38 +34,46 @@ const WaitingDriverScreen = () => {
 
   return (
     <View style={styles.container}>
-      <StatusBar style="light" />
-      <View style={styles.topNavbar}>
-      </View>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Flooded</Text>
-        <Text style={styles.headerDescription}>You are <Text style={{color: 'green'}}>SAFE</Text></Text>
-        <Text style={styles.headerDescription}>Nearest flood is happening in: <Text style={{color: 'red'}}>200km</Text></Text>
-      </View>
-      {initialRegion ? (
-        <MapView style={styles.map} initialRegion={initialRegion}>
-          {currentLocation && (
-            <Marker
-              coordinate={{
-                latitude: currentLocation.latitude,
-                longitude: currentLocation.longitude,
-              }}
-              title="Your Location"
-            />
-          )}
-        </MapView>
-      ) : (
-        <ActivityIndicator size="large" color="#242424" />
+      <StatusBar style="auto"/>
+
+      {activeTab === 0 && (
+        
+        initialRegion ? (
+          <MapView style={styles.map} initialRegion={initialRegion}>
+            {currentLocation && (
+              <Marker
+                coordinate={{
+                  latitude: currentLocation.latitude,
+                  longitude: currentLocation.longitude,
+                }}
+                title="Your Location"
+              />
+            )}
+          </MapView>
+        ) : (
+          <ActivityIndicator size="large" color="#242424" />
+        )
+      )}
+      {activeTab === 1 && (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Navigation Tab</Text>
+        </View>
+      )}
+
+      {activeTab === 2 && (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>More Tab</Text>
+        </View>
       )}
       <View style={styles.bottomNavbar}>
-        <TouchableOpacity style={[styles.bottomNavbarTab, activeTab === 0 && styles.bottomNavbarTabActive]} onPress={() => setActiveTab(0)}>
-          <Text style={styles.bottomNavbarTabText}>Home</Text>
+        <TouchableOpacity style={styles.bottomNavbarTab} onPress={() => setActiveTab(0)}>
+          <Ionicons name="map" size={30} color={activeTab === 0 ? "#F1F6F8" : "#b0b0b0"} />
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.bottomNavbarTab, activeTab === 1 && styles.bottomNavbarTabActive]} onPress={() => setActiveTab(1)}>
-          <Text style={styles.bottomNavbarTabText}>Map</Text>
+        <TouchableOpacity style={styles.bottomNavbarTab} onPress={() => setActiveTab(1)}>
+          <Ionicons name="location" size={30} color={activeTab === 1 ? "#F1F6F8" : "#b0b0b0"} />
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.bottomNavbarTab, activeTab === 2 && styles.bottomNavbarTabActive]} onPress={() => setActiveTab(2)}>
-          <Text style={styles.bottomNavbarTabText}>More</Text>
+        <TouchableOpacity style={styles.bottomNavbarTab} onPress={() => setActiveTab(2)}>
+          <Ionicons name="ellipsis-horizontal" size={30} color={activeTab === 2 ? "#F1F6F8" : "#b0b0b0"} />
         </TouchableOpacity>
       </View>
     </View>
@@ -76,44 +85,29 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  topNavbar: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 150,
-    backgroundColor: "#071952",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  topNavbarText: {
-    fontSize: 18,
-    color: "#fff",
+    backgroundColor: "#F1F6F8",
   },
   header: {
     position: "absolute",
     top: 95,
     left: 32,
     zIndex: 1,
-    zIndex: 1000,
   },
   headerText: {
     fontSize: 32,
     fontWeight: "bold",
-    color: "#ffffff",
-    marginBottom: 30,
+    color: "#000000",
+    marginBottom: 10,
   },
   headerDescription: {
-    fontSize: 18,
-    marginBottom: 5,
+    fontSize: 20,
+    marginBottom: 3,
   },
-
   map: {
-    width: "90%",
-    height: "50%",
-    borderRadius: 10,
+    width: "100%",
+    height: "100%",
     overflow: "hidden",
+    zIndex: 0,
   },
 
   bottomNavbar: {
@@ -122,26 +116,15 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     height: 100,
-    backgroundColor: "#071952",
+    backgroundColor: "#21273D",
     flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
   },
   bottomNavbarTab: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  bottomNavbarTabActive: {
-    borderBottomWidth: 0,
-    borderBottomColor: "#fff",
-    marginBottom: 20,
-  },
-  bottomNavbarTabText: {
-    fontSize: 18,
-    color: "#fff",
-    marginBottom: 20,
+    paddingBottom: 20,
   },
 });
 
